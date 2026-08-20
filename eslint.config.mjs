a922a67523fa35525ec073df730ff3a1cca0ca02
Keketso <keketsoleu25@gmail.com>
2026-08-20 13:46:57 +0200

    fix: ignore database package in root lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,5 +5,5 @@ import nextTypeScript from "eslint-config-next/typescript";
 export default defineConfig([
   ...nextVitals,
   ...nextTypeScript,
-  globalIgnores([".next/**", "legacy/**"]),
+  globalIgnores([".next/**", "legacy/**", "packages/db/**"]),
 ]);
